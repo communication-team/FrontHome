14c1df406c1623c7177cc1a842e41fc1d9690789
MohmmadNada <mohammad.nada_97@yahoo.com>
2021-08-15 18:30:46 +0300

    to add logo

--- components/UsedComponent/Header.js
+++ components/UsedComponent/Header.js
@@ -1,4 +1,8 @@
 import Link from 'next/link'
+// import Image from 'next/image'
+import Image from "next/dist/client/image"
+
+import logo1 from '/public/assest/images/logo1.png'
 
 export default function HeaderUsed (props) {
     return(
@@ -7,6 +11,7 @@ export default function HeaderUsed (props) {
                 <span class="SearchHEader">
                     <a href="/">
                         <img src="https://syarahcdn.syarah.com/syarah/bundles/logo.svg" alt="" />
+                        {/* <Image styles={{width:50 }} src={logo1} alt="logo" /> */}
                     </a>
                 </span>
           <ul>

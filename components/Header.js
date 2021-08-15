4dee8e76129bca7b33bb2fab6c98228b6ab042f0
MohmmadNada <mohammad.nada_97@yahoo.com>
2021-08-15 12:08:04 +0300

    init new page

--- components/Header.js
+++ components/Header.js
@@ -1,15 +1,31 @@
+import Link from 'next/link'
+
 export default function Header (props) {
     return(
         <header class="siteHeader ">
             <div class="container innerHdr">
-                <span><a href="/"><img src="https://syarahcdn.syarah.com/syarah/bundles/logo.svg" alt="" /></a></span>
+                <span><a href="/"><img src="" alt="" /></a></span>
+                {/* for logo  */}
                 <ul>
-                    <li><a href="C:\\Users\\Odai\\Desktop\\New folder\\Newfolder\\forsell.html">سيارات جديدة</a></li>
-                    <li><a href="C:\\Users\\Odai\\Desktop\\New folder\\Newfolder\\forsell.html">سيارات مستعملة</a></li>
-                    <li><a class="onlineHdrLink" href="/post/search?is_online=1">سيارة أونلاين</a></li>
+                    <li>
+                    <Link href="/">
+                        <a>الصفحة الرئيسية </a>
+                    </Link>
+                    </li>
+                    <li>
+                        <Link href='/forsellnew'>
+                        <a>سيارات جديدة </a>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/forsellused">
+                        <a>سيارات مستعملة </a>
+                        </Link>
+                    </li>
                     <li class="sellCarNewBtn hideBtn"><a href="/post/add-post" class="redBtn">اعرض سيارتك للبيع</a></li>
                     <li><span class="sideMenuToggle "><img src="https://syarahcdn.syarah.com/syarah/bundles/menuToggle.svg" alt="" /></span>
-                    </li></ul>
+                    </li>
+                    </ul>
             </div>
         </header>
     );
---
+++ pages/forsellused.js
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import Head2 from '../components/Head2'
+import Body from '../components/Body'
+import Header from '../components/Header'
+export default function Home() {
+  return (
+    <div>
+      <Head2/>
+      <Header/>
+      {/* <Body/> */}
+      from for sell page
+    </div>
+  )
+}

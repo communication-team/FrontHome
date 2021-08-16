fc00c3d019bdc1cb49c3820203c7548e63d761c4
odai1990 <odai22odai78@gmail.com>
2021-08-16 23:54:13 +0300

    adding post page

--- components/DetailComponent/LeftDetail.js
+++ components/DetailComponent/LeftDetail.js
@@ -3,23 +3,23 @@ export default function ColumnContainerDetail(props){
     return(
  
         <div class="columnContainer">
-            <h1 class="movabileHeader">فورد اكسبدشن 2014 للبيع</h1>
+            <h1 class="movabileHeader">{props.info.title}</h1>
             <div class="priceContainer">
                 <div>
                     <span styles="color: rgb(0, 182, 88);"></span>
                     <span>السعر</span>
-                    <strong class="priceBeforeTax">58,500<span>ريال</span></strong>
+                    <strong class="priceBeforeTax">{props.info.price}<span>دينار</span></strong>
                 </div>
             </div>
 
             <div class="contaictButtons">
                 <a class="whPV blueBtn" href="tel:+966505456784">
-                <img width="24" src="https://syarahcdn.syarah.com/syarah/bundles/Call.svg" alt=""/>اتصل</a>
-                <a class="whPV greenBtn" href="https://api.whatsapp.com/send?phone=966505456784">
+                <img width="24" src="https://syarahcdn.syarah.com/syarah/bundles/Call.svg" alt=""/>{props.info.phone}</a>
+                <a style ={{direction:'ltr'}}class="whPV greenBtn" href="https://api.whatsapp.com/send?phone=966505456784">
                 <img width="24" src="https://syarahcdn.syarah.com/syarah/bundles/wpImg.svg" alt=""/>واتساب</a>
             </div>
             <div class="sellerInfo">
-                <div class="randomPersonSeller"><span>الأردن - عمان</span>
+                <div class="randomPersonSeller"><span>{props.info.location +' - الاردن'} </span>
                 </div>
             </div>
             <div class="reportContainer ">
